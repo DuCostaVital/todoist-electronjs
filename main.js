80b69f9dcd8a38b31503204eabda5f8d4c25f348
DuCostaVital <ivair.vital@outlook.com>
2022-04-26 21:41:09 -0300

    Adicionando o icone de remover

--- main.js
+++ main.js
@@ -6,17 +6,17 @@ require('electron-reload')(__dirname, {
   electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
 });
 
-
 const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      icon: `${__dirname}/assets/icone.png`
-    })
-  
-    win.loadFile('index.html')
-  }
 
-  app.whenReady().then(() => {
-    createWindow()
+  const win = new BrowserWindow({ 
+    width: 800,
+    height: 600,
+    icon:`${__dirname}/assets/icon.png`
   })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
--- todo-list.js
+++ todo-list.js
@@ -1,24 +1,43 @@
-function adicionarItem (event) {
+const adicionarItemPelaTecla = (event) => {
+  //Pegando o tipo da tecla pelo evento
+  const tecla = event.key;
 
-  if (event && event.keycode !== 13) return;
+  //Verificando se a tecla pressionada é o Enter
+  if (tecla === "Enter") {
+    //Chamando a função responsável por adicionar item
+    adicionarItem();
 
-  const valorInput = document.getElementById("resultado").value
-  
-  //pegando a tag Ul do nosso HTMl pelo ID
-  const minhatagUL = document.getElementById("lista-de-tarefas");
+    //encerra o IF, porém não está sendo utilizado aqui, pois não temos nada abaixo do IF, apenas por demonstração
+    return;
+  }
+};
 
-   // Criando tag LI com JavaScript
-  const criarTagLi = document.createElement("li");
+adicionarItem = () => {
+  const valorInput = document.getElementById("oi").value;
 
-  // Adicionando um texto para nossa tag li criada
-  criarTagLi.innerText = valorInput;
+  //Pegado a tag UL do nosso HTML pelo ID
+  const minhaTagUl = document.getElementById("lista-de-tarefas");
 
-  // Adicionando a tag li para nossa ul
-  minhatagUL.appendChild(criarTagLi);
+  //Criando a tag LI com JavaScript
+  const criarTagLI = document.createElement("li");
 
-  console.log(criarTagLi);
+  //Criando uma tag em negrito
+  const tagRemover = `<i  onclick='removerItem(event)' class="fas fa-minus-circle"></i>`;
 
-  
- console.log('Entrou na função');
+  //Adicionando um texto para nossa tag li criada
+  criarTagLI.innerHTML = valorInput + tagRemover;
 
+  minhaTagUl.appendChild(criarTagLI);
+};
+
+function removerItem(event) {
+  //Pegando a tag <b> dentro do nosso evento
+
+  const meuIcone = event.target;
+
+  //pegando  a tag <Li> atrás vez da minha tag <i> com propriedade parentElement
+  const minhaLi = meuIcone.parentElement;
+
+  // excluindo a tag <Li> com a função remove()
+  minhaLi.remove();
 }
